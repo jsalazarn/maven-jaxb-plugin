f3e3a3a379571c5210e966877ba7de59940db705
jimmysn <jimmyr.salazar@gmail.com>
2020-08-07 16:51:36 -0500

    First Commit

---
+++ maven-jaxb-plugin/src/main/java/com/jimmysalazar/maven/MavenJaxbPluginApplication.java
@@ -0,0 +1,13 @@
+package com.jimmysalazar.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenJaxbPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenJaxbPluginApplication.class, args);
+	}
+
+}
